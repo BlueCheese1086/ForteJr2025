21be3bc1fd2f7b7eea23658323fd374c9467cec9
Turbojax07 <turbojax07@gmail.com>
2025-04-09 09:30:22 -0400

    Replacing the xbox remote with a joystick because there weren't any at PCS to test with.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,6 +3,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.RobotMap;
@@ -13,7 +14,7 @@ import frc.robot.subsystems.shooter.Commands.*;
 
 public class RobotContainer {
     // Replace with CommandPS4Controller or CommandJoystick if needed
-    private CommandXboxController controller = new CommandXboxController(0);
+    private CommandJoystick controller = new CommandJoystick(0);
 
     // Subsystems
     private Drivetrain drivetrain;
@@ -44,11 +45,11 @@ public class RobotContainer {
      * joysticks}.
      */
     private void configureBindings() {
-        drivetrain.setDefaultCommand(new ArcadeDrive(drivetrain, controller::getLeftY, controller::getRightX));
+        drivetrain.setDefaultCommand(new ArcadeDrive(drivetrain, controller::getY, controller::getZ));
 
-        controller.a().whileTrue(new SetFeedPercent(shooter, 1));
-        controller.b().whileTrue(new SetLaunchPercent(shooter, -1)).whileTrue(new SetFeedPercent(shooter, -1));
-        controller.y().toggleOnTrue(new SetLaunchPercent(shooter, 1));
+        controller.button(1).whileTrue(new SetFeedPercent(shooter, 1));
+        controller.button(3).whileTrue(new SetLaunchPercent(shooter, -1)).whileTrue(new SetFeedPercent(shooter, -1));
+        controller.button(2).toggleOnTrue(new SetLaunchPercent(shooter, 1));
     }
 
     /**
